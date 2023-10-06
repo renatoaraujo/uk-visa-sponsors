ae959a54b253511a2f0ac34531ce2467659ff02a
Renato Rodrigues de Araujo <renato.r.araujo@gmail.com>
2023-10-06 18:05:19 +0100

    separate the logic to find the datasource

--- pkg/scraper.go
+++ pkg/scraper.go
@@ -18,20 +18,16 @@ func NewScraper(url string) *Scraper {
 	}
 }
 
-func (s *Scraper) findDataSource() {
-
-}
-
-func (s *Scraper) FetchData() error {
+func (s *Scraper) findDataSource() (string, error) {
 	resp, err := http.Get(s.url)
 	if err != nil {
-		return fmt.Errorf("error getting content from %s: %v", s.url, err)
+		return "", fmt.Errorf("error getting content from %s: %v", s.url, err)
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return fmt.Errorf("error reading response body: %v", err)
+		return "", fmt.Errorf("error reading response body: %v", err)
 	}
 
 	content := string(body)
@@ -40,16 +36,26 @@ func (s *Scraper) FetchData() error {
 	pattern := `https?://[^"'\s]+\.csv`
 	re, err := regexp.Compile(pattern)
 	if err != nil {
-		return fmt.Errorf("error compiling regex: %v", err)
+		return "", fmt.Errorf("error compiling regex: %v", err)
 	}
 
 	// return the first one or fail
 	match := re.FindString(content)
 	if match == "" {
-		return errors.New("no csv link found")
+		return "", errors.New("no csv link found")
 	}
 
-	fmt.Print(match)
+	return match, nil
+}
+
+func (s *Scraper) FetchData() error {
+	ds, err := s.findDataSource()
+	if err != nil {
+		return err
+	}
+	output := fmt.Sprintf("found datasource link: %s", ds)
+
+	fmt.Println(output)
 
 	return nil
 }
